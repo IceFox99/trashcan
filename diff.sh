30b4c147f2504e52da7e0647e7097c1e0483c59c
IceFox99 <huyuefeng99@gmail.com>
2023-06-04 19:43:26 +0900

    Update tmux colour config and force color prompt.

---
+++ diff.sh
@@ -0,0 +1,4 @@
+git diff bash_config/.bashrc ~/.bashrc
+git diff bash_config/.tmux.conf ~/.tmux.conf
+git diff spacevim_config/init.toml ~/.SpaceVim.d/init.toml
+git diff spacevim_config/init.vim ~/.SpaceVim/init.vim
